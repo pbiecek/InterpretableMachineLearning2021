fd9bae744ac32da49705dc3c85395aba4db04ac4
a.bukala <a.bukala@cent.uw.edu.pl>
2021-04-16 14:46:31 +0200

    Add 4th homework

---
+++ Homeworks/HW4/AdrianaBukala/_plot_files/obs7.js
@@ -0,0 +1,15 @@
+observation = [
+  {
+    "Pclass": 3,
+    "Sex": "female",
+    "Age": 47.0,
+    "SibSp": 1,
+    "Parch": 0,
+    "Fare": 14.5,
+    "Cabin": "None",
+    "_yhat_": 0.32249458843997175,
+    "_label_": "GradientBoosting",
+    "_ids_": 0,
+    "_y_": 0
+  }
+];
